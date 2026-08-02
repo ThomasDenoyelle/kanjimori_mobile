4417a034be4ba85dab719def4f8a7d6a401e0226
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-02 02:50:12 +0200

    Add SplashScreen for initial app loading and login status check

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import '../services/auth_service.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final AuthService _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkLoginStatus();
+
+  }
+
+  Future<void>_checkLoginStatus() async {
+      final bool isLoggedIn = await _authService.isLoggedIn();
+
+      if (!mounted) return;
+
+      if (isLoggedIn) {
+        Navigator.pushReplacement(
+          context, 
+          MaterialPageRoute(builder: (context) => const HomeScreen())
+        );
+      } else {
+        Navigator.pushReplacement(
+          context, 
+          MaterialPageRoute(builder: (context) => const LoginScreen())
+        );
+      }
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const CircularProgressIndicator(),
+      )
+    );
+  }
+}
