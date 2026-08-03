8ef7032669bd84e19931dc8fc02cf65e81df53d2
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-03 02:24:28 +0200

    Refactor HomeScreen to StatefulWidget and implement quiz fetching with FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:kanji_mobile/services/auth_service.dart';
 import 'login_screen.dart';
+import '../services/api_service.dart';
+import '../models/quiz.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService _apiService = ApiService();
+  late Future<List<Quiz>> _quizzesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _quizzesFuture = _apiService.fetchQuizzes();
+  }
 
   Future<void> _confirmLogout(BuildContext context) async {
     final bool? confirm = await showDialog<bool>(
@@ -57,8 +73,37 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
         ),
-      body: const Center(
-        child: Text('Bienvenue dans ton application ! 🎉', style: TextStyle(fontSize: 20)),
+      body: FutureBuilder<List<Quiz>>(
+  future: _quizzesFuture,
+  builder: (context, snapshot) {
+    
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    if (snapshot.hasError) {
+      return Center(child: Text('Erreur : ${snapshot.error}'));
+    }
+    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+      return const Center(child: Text('Aucun quiz disponible.'));
+    }
+    
+    final quizzes = snapshot.data!;
+    
+    return ListView.builder(
+      itemCount: quizzes.length,
+      itemBuilder: (context, index) {
+        final quiz = quizzes[index];
+
+        return ListTile(
+          title: Text(quiz.title),
+          subtitle: Text('${quiz.questions.length} questions'),
+          leading: Icon(Icons.quiz),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: () { print('Clic sur le quiz ${quiz.id}'); }
+        );
+      }
+      ); 
+  },
       ),
     );
   }
