a8e6ac48c3fd15cd8bfbbda1b24c9e8b83da4098
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-03 03:20:06 +0200

    Add fetchMyAttempts method to ApiService for retrieving user quiz attempts

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:kanji_mobile/models/quiz.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:kanji_mobile/models/quiz_attempt.dart';
 
 class ApiService {
   final _storage = const FlutterSecureStorage();
@@ -37,6 +38,51 @@ class ApiService {
         }
 
         return listToParse.map((json) => Quiz.fromJson(json)).toList();
+        
+      } else {
+        print('Erreur serveur : ${response.statusCode}');
+        return [];
+      }
+      
+    } catch (e) {
+      print("Erreur lors de la récupération des quiz : $e");
+      return [];
+    }
+    
+  }
+
+
+  Future<List<QuizAttempt>> fetchMyAttempts() async {
+    final token = await _storage.read(key: 'jwt');
+
+    final url = Uri.parse('http://192.168.1.41:8080/api/quiz_attempts');
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/ld+json',
+        },
+      ).timeout(const Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        
+        final decodedBody = jsonDecode(response.body);
+
+        List<dynamic> listToParse = [];
+
+        if (decodedBody is List) {
+          listToParse = decodedBody;
+        } else if (decodedBody is Map && decodedBody.containsKey('member')) {
+          listToParse = decodedBody['member'];
+        } 
+        else {
+          print('Format de réponse non reconnu');
+          return [];
+        }
+
+        return listToParse.map((json) => QuizAttempt.fromJson(json)).toList();
         
       } else {
         print('Erreur serveur : ${response.statusCode}');
