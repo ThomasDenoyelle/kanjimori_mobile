8ebab7845364360d9da04bed9c8cef259a8805ae
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-03 03:30:06 +0200

    Add answersCount field to QuizAttempt model and update JSON parsing

--- lib/models/quiz_attempt.dart
+++ lib/models/quiz_attempt.dart
@@ -6,22 +6,27 @@ class QuizAttempt {
   final int maxScore;
   final String mode;
   final Quiz quiz;
+  final int answersCount;
 
   QuizAttempt({
     required this.id,
     required this.score,
     required this.maxScore,
     required this.mode,
-    required this.quiz
+    required this.quiz,
+    required this.answersCount
   });
 
   factory QuizAttempt.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> answers = json['answerAttempts'] ?? [];
+
     return QuizAttempt(
       id: json['id'] as int,
       score: json['score'] as int,
       maxScore: json['maxScore'] as int,
       mode: json['mode'] as String,
-      quiz: Quiz.fromJson(json['quiz'])
+      quiz: Quiz.fromJson(json['quiz']),
+      answersCount: answers.length,
     );
   }
 }
