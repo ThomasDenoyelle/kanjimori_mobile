75119da5df191f0dcd823ee021daafbaed4f40c8
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-03 02:05:04 +0200

    Add ApiService class to fetch quizzes from the API with JWT authentication

---
+++ lib/services/api_service.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:kanji_mobile/models/quiz.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final _storage = const FlutterSecureStorage();
+
+  Future<List<Quiz>> fetchQuizzes() async {
+    final token = await _storage.read(key: 'jwt');
+
+    final url = Uri.parse('http://192.168.1.41:8080/api/quizzes');
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/ld+json',
+        },
+      ).timeout(const Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        
+        final decodedBody = jsonDecode(response.body);
+
+        List<dynamic> listToParse = [];
+
+        if (decodedBody is List) {
+          listToParse = decodedBody;
+        } else if (decodedBody is Map && decodedBody.containsKey('member')) {
+          listToParse = decodedBody['member'];
+        } 
+        else {
+          print('Format de réponse non reconnu');
+          return [];
+        }
+
+        return listToParse.map((json) => Quiz.fromJson(json)).toList();
+        
+      } else {
+        print('Erreur serveur : ${response.statusCode}');
+        return [];
+      }
+      
+    } catch (e) {
+      print("Erreur lors de la récupération des quiz : $e");
+      return [];
+    }
+    
+  }
+}
