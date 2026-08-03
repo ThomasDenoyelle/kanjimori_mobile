1378293cef96ccddfa715d4c0b5aabb4cd3af927
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-03 14:33:24 +0200

    Refactor main.dart to update theme color scheme and remove commented code

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'KanjiMori',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+
+          primary: Color(0xFF7C3AED),
+          onPrimary: Colors.white,
+
+          secondary: Color(0xFFA78BFA),
+          onSecondary: Colors.white,
+
+          error: Colors.redAccent,
+          onError: Colors.white,
+
+          surface: Color(0xFF1D232A),
+          onSurface: Colors.white,
+
+          primaryContainer: Color(0xFF2B2344),
+          onPrimaryContainer: Colors.white,
+
+          secondaryContainer: Color(0xFF1F2333),
+          onSecondaryContainer: Colors.white,
+        ),
       ),
       home: const SplashScreen(),
     );
