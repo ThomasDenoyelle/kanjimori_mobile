a1ea5799b142082bb69f9bc00b21592c07009c13
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-05 00:38:39 +0200

    Add PublicQuizzesView widget for displaying public quizzes

---
+++ lib/screens/quiz_tabs/public_quizzes_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PublicQuizzesView extends StatefulWidget {
+  const PublicQuizzesView({super.key});
+
+  @override
+  State<PublicQuizzesView> createState() => _PublicQuizzesViewState();
+}
+
+class _PublicQuizzesViewState extends State<PublicQuizzesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
