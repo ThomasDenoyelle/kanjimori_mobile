97d8a530aee2c4d7d05e4f55fc14ca8f2a19e2d2
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-08-05 00:38:34 +0200

    Enhance fetchQuizzes method to accept query parameters for author, title, and visibility; add getCurrentUser method for token validation

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:kanji_mobile/models/quiz.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -7,10 +9,26 @@ import 'package:kanji_mobile/models/quiz_attempt.dart';
 class ApiService {
   final _storage = const FlutterSecureStorage();
 
-  Future<List<Quiz>> fetchQuizzes() async {
+  Future<List<Quiz>> fetchQuizzes(String? authorUrl, String? searchTitle, bool? isPublic) async {
     final token = await _storage.read(key: 'jwt');
 
-    final url = Uri.parse('http://192.168.1.41:8080/api/quizzes');
+    final Map<String, String> queryParameters = {};
+
+    if (authorUrl != null) {
+      queryParameters['author'] = authorUrl;
+    }
+    if (searchTitle != null) {
+      queryParameters['title'] = searchTitle;
+    }
+    if (isPublic != null) {
+      queryParameters['isPublic'] = isPublic.toString(); 
+    }
+
+    final url = Uri.http(
+      '192.168.1.41:8080', 
+      '/api/quizzes', 
+      queryParameters
+    );
 
     try {
       final response = await http.get(
@@ -94,5 +112,19 @@ class ApiService {
       return [];
     }
     
+  }
+
+  Future<Map<String, dynamic>?> getCurrentUser() async {
+    String? token = await _storage.read(key: 'jwt');
+    if (token == null) return null;
+
+    bool isExpired = JwtDecoder.isExpired(token);
+    if (isExpired) {
+      await _storage.delete(key: 'auth_token');
+      return null;
+    }
+
+    Map<String, dynamic> payload = JwtDecoder.decode(token);
+    return payload;
   }
 }
